b16c158ba8c0a300068a915fc2929494de890c65
rohan <rohannayakwadi@gmail.com>
2019-10-15 09:46:07 -0500

    Update main.go

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if found != "" {
 		name = found
 	} else {
-		name = "Jenkins"
+		name = "World"
 	}
-	fmt.Fprintf(w, "Welcome to  %s!", name)
+	fmt.Fprintf(w, "Hello %s!", name)
 }
 
 func main() {
